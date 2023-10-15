6e30f0679fe13d7f6ad39c854dc3cbc6ee84fb7f
CJeezy <cjduma98@gmail.com>
2023-10-15 18:30:08 -0400

    header changes

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -48,32 +48,6 @@ const Header = () => {
         </div>
       </motion.div>
         
-      <motion.div
-        whileInView={{ opacity: [0, 1]}}
-        transition={{duration: 1.5, delayChildren: 0.5}}
-        className='app__header-img'
-      >
-        <img src={images.profile} alt='profile_bg'/>
-        <motion.img
-          whileInView={{ scale: [0, 1]}}
-          transition={{duration: 2, ease: 'easeInOut'}}
-          src={images.circle} alt='profile_circ'
-          className='overlay_circle'
-        />     
-
-      </motion.div>
-
-      <motion.div
-        variant={scaleVariance}
-        whileInView={scaleVariance.whileInView}
-        className='app__header-circles'
-        >
-           {[images.react, images.javascript, images.cpp].map((circle, index ) => (
-            <div className='circle-cmp app__flex' key={'circle-${index}'}>
-              <img src={circle} alt='circle'/>
-            </div>
-           ))}
-      </motion.div>
 
     </div>
   )
